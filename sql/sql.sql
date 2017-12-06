6148405fe1bf857a87b3efcf58e1a7b764ac0309
lewis.liu <1019175915@qq.com>
2017-12-07 00:21:19 +0800

    Update sql

--- sql/sql.sql
+++ sql/sql.sql
@@ -6,51 +6,56 @@ USE `course_selection`;
 
 -- 创建表
 -- 权限表
-DROP TABLE IF EXISTS `authority`;
+DROP TABLE IF EXISTS `cs_authority`;
 
-CREATE TABLE `authority`(
+CREATE TABLE `cs_authority`(
   `id` bigint NOT NULL auto_increment COMMENT '权限ID',
   `name` varchar(50) NOT NULL COMMENT '权限名称',
   primary key (`id`)
 )ENGINE=InnoDB auto_increment=1000 DEFAULT CHARSET=utf8 COMMENT='权限表';
 
--- 用户类型表
-DROP TABLE IF EXISTS `user_type`;
+-- 角色表/用户类型表
+DROP TABLE IF EXISTS `cs_role`;
 
-CREATE TABLE `user_type`(
-  `id` bigint NOT NULL auto_increment COMMENT '用户类型ID',
-  `name` varchar(50) NOT NULL COMMENT '用户类型名称',
+CREATE TABLE `cs_role`(
+  `id` bigint NOT NULL auto_increment COMMENT '角色ID',
+  `name` varchar(50) NOT NULL COMMENT '角色名称',
   primary key (`id`)
-)ENGINE=InnoDB auto_increment=1000 DEFAULT CHARSET=utf8 COMMENT='用户类型表';
+)ENGINE=InnoDB auto_increment=1000 DEFAULT CHARSET=utf8 COMMENT='角色表';
 
--- 用户权限表
-DROP TABLE IF EXISTS `user_authority`;
+-- 角色权限表
+DROP TABLE IF EXISTS `cs_role_authority`;
 
-CREATE TABLE `user_authority`(
+CREATE TABLE `cs_role_authority`(
   `id` bigint NOT NULL auto_increment COMMENT '用户权限ID',
-  `name` varchar(50) NOT NULL COMMENT '用户权限名称',
-  `user_type` varchar(50) NOT NULL COMMENT '用户类型',
+  `role` varchar(50) NOT NULL COMMENT '角色/用户类型',
+  `authority` varchar(50) NOT NULL COMMENT '权限名称',
   primary key (`id`),
-  key `idx_user_type` (`user_type`),
-  CONSTRAINT `fk_userAuthority_userType` FOREIGN KEY (`user_type`) REFERENCES `user_type` (`name`)
-)ENGINE=InnoDB auto_increment=1000 DEFAULT CHARSET=utf8 COMMENT='用户权限表';
+  key `idx_role` (`role`),
+  key `idx_authority` (`authority`),
+  CONSTRAINT `fk_roleAuthority_role` FOREIGN KEY (`role`) REFERENCES `cs_role` (`name`),
+  CONSTRAINT `fk_roleAuthority_authority` FOREIGN KEY (`authority`) REFERENCES `cs_authority` (`name`)
+)ENGINE=InnoDB auto_increment=1000 DEFAULT CHARSET=utf8 COMMENT='角色权限表';
 
 -- 学校表
-DROP TABLE IF EXISTS `school`;
+DROP TABLE IF EXISTS `cs_school`;
 
-CREATE TABLE `school`(
+CREATE TABLE `cs_school`(
   `id` bigint NOT NULL auto_increment COMMENT '学校ID',
   `name` varchar(50) NOT NULL COMMENT '学校名称',
+  `description` text COMMENT '学校描述',
   primary key(`id`)
 )ENGINE=InnoDB auto_increment=1000 DEFAULT CHARSET=utf8 COMMENT='学校表';
 
--- 学校院系表
-DROP TABLE IF EXISTS `dept`;
+-- 院系表
+DROP TABLE IF EXISTS `cs_dept`;
 
-CREATE TABLE `dept`(
+CREATE TABLE `cs_dept`(
   `id` bigint NOT NULL auto_increment COMMENT '院系ID',
   `name` varchar(50) NOT NULL COMMENT '院系名称',
   `school_id` bigint NOT NULL COMMENT '学校ID',
+  `description` text COMMENT '院系描述',
   primary key(`id`),
-  
-)ENGINE=InnoDB auto_increment=1000 DEFAULT CHARSET=utf8 COMMENT='学校院系表';
+  key `idx_schoolId`
+  CONSTRAINT `fk_dept_school` FOREIGN KEY (`school_id`) REFERENCES `cs_school` (`id`)
+)ENGINE=InnoDB auto_increment=1000 DEFAULT CHARSET=utf8 COMMENT='院系表';
