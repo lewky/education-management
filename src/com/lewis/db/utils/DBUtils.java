363836e84000f06b90ebe64b59e9d1c7484fe12d
lewis.liu <1019175915@qq.com>
2018-01-12 23:55:23 +0800

    add slf4j+logback & dbcfg file

---
+++ src/com/lewis/db/utils/DBUtils.java
@@ -0,0 +1,74 @@
+// Copyright (c) 2018-2018 "Lewis" Limited. All rights reserved.
+// ============================================================================
+// CURRENT VERSION EA.1.0.0
+// ============================================================================
+// CHANGE LOG
+// EA.1.0.0 : 2018-1-12, "Lewis" created
+// ============================================================================
+package com.lewis.db.utils;
+
+import java.util.ResourceBundle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * utils for db
+ *
+ * @author "Lewis"
+ */
+public class DBUtils {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DBUtils.class);
+
+    // keys in the db config properties
+    private static final String KEY_DRIVER_CLASS = "driverClass";
+    private static final String KEY_URL = "url";
+    private static final String KEY_JDBC_USER = "jdbc.user";
+    private static final String KEY_PASSWORD = "password";
+
+    /**
+     * db config file
+     */
+    private static final String DB_CONFIG_NAME = "dbcfg";
+    /**
+     * driver class for jdbc
+     */
+    private static final String DRIVER_CLASS;
+    /**
+     * url for jdbc
+     */
+    private static final String URL;
+    /**
+     * user for jdbc
+     */
+    private static final String JDBC_USER;
+    /**
+     * password for jdbc
+     */
+    private static final String PASSWORD;
+
+    static {
+        // get values form the db config file by the keys
+        final ResourceBundle rb = ResourceBundle.getBundle(DB_CONFIG_NAME);
+        DRIVER_CLASS = rb.getString(KEY_DRIVER_CLASS);
+        URL = rb.getString(KEY_URL);
+        JDBC_USER = rb.getString(KEY_JDBC_USER);
+        PASSWORD = rb.getString(KEY_PASSWORD);
+
+        // load jdbc driver class
+        try {
+            Class.forName(DRIVER_CLASS);
+        } catch (final ClassNotFoundException e) {
+            LOGGER.error("Fail to load jdbc driver class.", e);
+        }
+    }
+
+    // private constructor
+    private DBUtils() {
+    }
+
+    public static void main(final String[] args) {
+
+    }
+}
