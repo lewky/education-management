70b3b8eb42ae1cab93df94040e1b008fc5843675
lewis.liu <1019175915@qq.com>
2018-03-14 23:49:30 +0800

    update sql

--- sql/sql.sql
+++ sql/sql.sql
@@ -11,10 +11,10 @@ DROP TABLE IF EXISTS `ea_authority`;
 CREATE TABLE `ea_authority`(
   `id` bigint unsigned NOT NULL COMMENT '主键ID',
   `name` varchar(30) NOT NULL COMMENT '权限名称',
-  `code` varchar(100) NOT NULL COMMENT '权限编码',
+  `code` varchar(100) NOT NULL COMMENT '权限编码:ea.{entity}.{action}',
   `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_on` TIMESTAMP DEFAULT NULL COMMENT '更新时间',
-  `updated_by` varchar(30) NULL COMMENT '被谁更新',
+  `updated_by` varchar(30) DEFAULT NULL COMMENT '被谁更新',
   primary key (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='权限表';
 
@@ -27,7 +27,7 @@ CREATE TABLE `ea_role`(
   `is_valid` tinyint DEFAULT '0' COMMENT '角色是否有效, 0:无效 1:有效',
   `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_on` TIMESTAMP DEFAULT NULL COMMENT '更新时间',
-  `updated_by` varchar(30) NULL COMMENT '被谁更新',
+  `updated_by` varchar(30) DEFAULT NULL COMMENT '被谁更新',
   primary key (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色表';
 
@@ -41,7 +41,7 @@ CREATE TABLE `ea_role_authority`(
   `is_valid` tinyint DEFAULT '0' COMMENT '状态标识, 0:无权限 1:有权限',
   `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_on` TIMESTAMP DEFAULT NULL COMMENT '更新时间',
-  `updated_by` varchar(30) NULL COMMENT '被谁更新',
+  `updated_by` varchar(30) DEFAULT NULL COMMENT '被谁更新',
   primary key (`id`),
   CONSTRAINT `fk_roleAuthority_role` FOREIGN KEY (`role_id`) REFERENCES `ea_role` (`id`),
   CONSTRAINT `fk_roleAuthority_authority` FOREIGN KEY (`authority_id`) REFERENCES `ea_authority` (`id`)
@@ -72,7 +72,7 @@ CREATE TABLE `ea_user` (
   `last_login` TIMESTAMP DEFAULT NULL COMMENT '上次登录时间',
   `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_on` TIMESTAMP DEFAULT NULL COMMENT '更新时间',
-  `updated_by` varchar(30) NULL COMMENT '被谁更新',
+  `updated_by` varchar(30) DEFAULT NULL COMMENT '被谁更新',
   PRIMARY KEY (`id`),
   key `idx_login_id` (`login_id`),
   CONSTRAINT `fk_user_role` FOREIGN KEY (`role_id`) REFERENCES `ea_role` (`id`)
@@ -83,15 +83,15 @@ DROP TABLE IF EXISTS `ea_school`;
 
 CREATE TABLE `ea_school`(
   `id` bigint unsigned NOT NULL COMMENT '主键ID',
-  `school_no` bigint NOT NULL auto_increment COMMENT'学校编号',
+  `school_no` bigint unsigned NOT NULL auto_increment COMMENT'学校编号',
   `name` varchar(30) NOT NULL COMMENT '学校名称',
-  `president` varchar(30) DEFAULT NULL COMMENT '校长',
+  `president_name` varchar(30) DEFAULT NULL COMMENT '校长',
   `president_id` bigint unsigned DEFAULT NULL COMMENT '校长ID',
   `description` text DEFAULT NULL COMMENT '学校描述',
   `place` varchar(120) DEFAULT NULL COMMENT '学校地址',
   `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_on` TIMESTAMP DEFAULT NULL COMMENT '更新时间',
-  `updated_by` varchar(30) NULL COMMENT '被谁更新',
+  `updated_by` varchar(30) DEFAULT NULL COMMENT '被谁更新',
   primary key(`id`),
   key `idx_school_no` (`school_no`),
   CONSTRAINT `fk_school_user` FOREIGN KEY (`president_id`) REFERENCES `ea_user` (`login_id`)
@@ -102,7 +102,7 @@ DROP TABLE IF EXISTS `ea_dept`;
 
 CREATE TABLE `ea_dept`(
   `id` bigint unsigned NOT NULL COMMENT '主键ID',
-  `dept_no` bigint NOT NULL auto_increment COMMENT'院系编号',
+  `dept_no` bigint unsigned NOT NULL auto_increment COMMENT'院系编号',
   `name` varchar(30) NOT NULL COMMENT '院系名称',
   `school_name` varchar(30) NOT NULL COMMENT '学校名称',
   `school_id` bigint unsigned NOT NULL COMMENT '学校ID',
@@ -113,7 +113,7 @@ CREATE TABLE `ea_dept`(
   `telephone` varchar(15) DEFAULT NULL COMMENT '联系方式',
   `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_on` TIMESTAMP DEFAULT NULL COMMENT '更新时间',
-  `updated_by` varchar(30) NULL COMMENT '被谁更新',
+  `updated_by` varchar(30) DEFAULT NULL COMMENT '被谁更新',
   primary key(`id`),
   key `idx_dept_no` (`dept_no`),
   CONSTRAINT `fk_dept_school` FOREIGN KEY (`school_id`) REFERENCES `ea_school` (`id`),
@@ -125,7 +125,7 @@ DROP TABLE IF EXISTS `ea_major`;
 
 CREATE TABLE `ea_major` (
   `id` bigint unsigned NOT NULL COMMENT '主键ID',
-  `major_no` bigint NOT NULL auto_increment COMMENT'专业编号',
+  `major_no` bigint unsigned NOT NULL auto_increment COMMENT'专业编号',
   `name` varchar(30) NOT NULL COMMENT '专业名称',
   `dept_name` varchar(30) NOT NULL COMMENT '院系名称',
   `dept_id` bigint unsigned NOT NULL COMMENT '院系ID',
@@ -135,7 +135,7 @@ CREATE TABLE `ea_major` (
   `description` text DEFAULT NULL COMMENT '专业介绍',
   `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_on` TIMESTAMP DEFAULT NULL COMMENT '更新时间',
-  `updated_by` varchar(30) NULL COMMENT '被谁更新',
+  `updated_by` varchar(30) DEFAULT NULL COMMENT '被谁更新',
   PRIMARY KEY  (`id`),
   key `idx_major_no` (`major_no`),
   CONSTRAINT `idx_major_dept` FOREIGN KEY (`dept_id`) REFERENCES `ea_dept` (`id`),
@@ -156,7 +156,7 @@ CREATE TABLE `ea_class` (
   `adviser_id` bigint unsigned NOT NULL COMMENT '班主任ID',
   `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_on` TIMESTAMP DEFAULT NULL COMMENT '更新时间',
-  `updated_by` varchar(30) NULL COMMENT '被谁更新',
+  `updated_by` varchar(30) DEFAULT NULL COMMENT '被谁更新',
   PRIMARY KEY  (`id`),
   key `idx_class_no` (`class_no`),
   CONSTRAINT `idx_class_major` FOREIGN KEY (`major_id`) REFERENCES `ea_major` (`id`),
@@ -170,9 +170,10 @@ CREATE TABLE `ea_class_student` (
   `id` bigint unsigned NOT NULL COMMEMT '主键ID',
   `class_id` bigint unsigned NOT NULL COMMENT '班级ID',
   `student_id` bigint unsigned NOT NULL COMMENT '学生ID',
+  `student_name` varchar(30) NOT NULL COMMENT '学生姓名',
   `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_on` TIMESTAMP DEFAULT NULL COMMENT '更新时间',
-  `updated_by` varchar(30) NULL COMMENT '被谁更新',
+  `updated_by` varchar(30) DEFAULT NULL COMMENT '被谁更新',
   PRIMARY KEY  (`id`),
   CONSTRAINT `idx_class_student_class` FOREIGN KEY (`class_id`) REFERENCES `ea_class` (`id`),
   CONSTRAINT `idx_class_student_user` FOREIGN KEY (`student_id`) REFERENCES `ea_user` (`login_id`)
@@ -188,7 +189,7 @@ CREATE TABLE `ea_course` (
   `teacher_id` bigint unsigned NOT NULL COMMENT '教师ID',
   `school_year` smallint unsigned NOT NULL COMMENT '学年:YYYY',
   `semester` tinyint NOT NULL COMMENT '学期:1,2,3',
-  `period` smallint unsigned NOT NULL COMMENT '学时',
+  `period` float unsigned NOT NULL COMMENT '学时',
   `credit` float unsigned NOT NULL COMMENT '学分',
   `major_name` varchar(30) NOT NULL COMMENT '专业名称',
   `major_id` bigint unsigned NOT NULL COMMENT '专业ID',
@@ -200,12 +201,13 @@ CREATE TABLE `ea_course` (
   `course_type_code` varchar(30) NOT NULL COMMENT '课程类型代码',
   `course_type_name` varchar(30) NOT NULL COMMENT '课程类型名字:社会科学...',
   `is_minor` tinyint DEFAULT '0' COMMENT '是否是选修课, 0:必修课 1:选修课',
-  `is_validated` tinyint DEFAULT '0' COMMENT '是否已经验证, 0:未验证 1:已验证',
+  `is_validated` tinyint DEFAULT '0' COMMENT '是否已经审核, 0:未审核 1:已审核',
   `is_arranged` tinyint DEFAULT '0' COMMENT '是否已经排课, 0:未排课 1:已排课',
   `selected_num` smallint unsigned DEFAULT '0' COMMENT '选课人数',
+  `max_num` smallint unsigned DEFAULT '200' COMMENT '选课人数上限',
   `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_on` TIMESTAMP DEFAULT NULL COMMENT '更新时间',
-  `updated_by` varchar(30) NULL COMMENT '被谁更新',
+  `updated_by` varchar(30) DEFAULT NULL COMMENT '被谁更新',
   PRIMARY KEY  (`id`),
   CONSTRAINT `idx_course_major` FOREIGN KEY (`major_id`) REFERENCES `ea_major` (`id`),
   CONSTRAINT `idx_course_user` FOREIGN KEY (`teacher_id`) REFERENCES `ea_user` (`login_id`)
@@ -217,10 +219,12 @@ DROP TABLE IF EXISTS `ea_student_course`;
 CREATE TABLE `ea_student_course` (
   `id` bigint unsigned NOT NULL COMMEMT '主键ID',
   `course_id` bigint unsigned NOT NULL COMMENT '课程ID',
+  `course_name` varchar(30) NOT NULL COMMENT '课程名称',
   `student_id` bigint unsigned NOT NULL COMMENT '学生ID',
+  `student_name` varchar(30) NOT NULL COMMENT '学生姓名',
   `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_on` TIMESTAMP DEFAULT NULL COMMENT '更新时间',
-  `updated_by` varchar(30) NULL COMMENT '被谁更新',
+  `updated_by` varchar(30) DEFAULT NULL COMMENT '被谁更新',
   PRIMARY KEY  (`id`),
   CONSTRAINT `idx_student_course_course` FOREIGN KEY (`course_id`) REFERENCES `ea_course` (`id`),
   CONSTRAINT `idx_student_course_user` FOREIGN KEY (`student_id`) REFERENCES `ea_user` (`login_id`)
@@ -232,13 +236,13 @@ DROP TABLE IF EXISTS `ea_score`;
 CREATE TABLE `ea_score` (
   `id` bigint unsigned NOT NULL COMMEMT '主键ID',
   `parent_id` bigint unsigned NOT NULL COMMEMT '学生选课ID',
-  `regular_score` smallint default NULL COMMENT '平时成绩',
-  `midterm_score` smallint default NULL COMMENT '期中成绩',
-  `terminal_score` smallint default NULL COMMENT '期末成绩',
-  `score` smallint default NULL COMMENT '综合成绩',
+  `regular_score` float unsigned DEFAULT NULL COMMENT '平时成绩',
+  `midterm_score` float unsigned DEFAULT NULL COMMENT '期中成绩',
+  `terminal_score` float unsigned DEFAULT NULL COMMENT '期末成绩',
+  `score` float unsigned DEFAULT NULL COMMENT '综合成绩',
   `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_on` TIMESTAMP DEFAULT NULL COMMENT '更新时间',
-  `updated_by` varchar(30) NULL COMMENT '被谁更新',
+  `updated_by` varchar(30) DEFAULT NULL COMMENT '被谁更新',
   PRIMARY KEY  (`id`),
   CONSTRAINT `idx_score_student_course` FOREIGN KEY (`parent_id`) REFERENCES `ea_student_course` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='成绩表';
@@ -253,7 +257,7 @@ CREATE TABLE `ea_codelist_book` (
   `description` text DEFAULT NULL COMMENT '介绍',
   `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_on` TIMESTAMP DEFAULT NULL COMMENT '更新时间',
-  `updated_by` varchar(30) NULL COMMENT '被谁更新',
+  `updated_by` varchar(30) DEFAULT NULL COMMENT '被谁更新',
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB auto_increment=1 DEFAULT CHARSET=utf8 COMMENT='Codelist Book表';
 
@@ -268,7 +272,7 @@ CREATE TABLE `ea_codelist` (
   `description` text DEFAULT NULL COMMENT '介绍',
   `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_on` TIMESTAMP DEFAULT NULL COMMENT '更新时间',
-  `updated_by` varchar(30) NULL COMMENT '被谁更新',
+  `updated_by` varchar(30) DEFAULT NULL COMMENT '被谁更新',
   PRIMARY KEY  (`id`),
   CONSTRAINT `idx_codelist_codelist_book` FOREIGN KEY (`parent_id`) REFERENCES `ea_codelist_book` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Codelist表';
