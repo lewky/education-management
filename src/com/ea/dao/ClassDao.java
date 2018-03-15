c1c57a2e49648165ebd5c547e4b6aad96bdabb9c
lewis.liu <1019175915@qq.com>
2018-03-15 23:49:55 +0800

    add dao for every entity

---
+++ src/com/ea/dao/ClassDao.java
@@ -0,0 +1,20 @@
+// Copyright (c) 2018-2018 Lewis.Liu Limited. All rights reserved.
+// ============================================================================
+// CURRENT VERSION EA.1.0.0
+// ============================================================================
+// CHANGE LOG
+// EA.1.0.0 : 2018-3-15, Lewis.Liu created
+// ============================================================================
+package com.ea.dao;
+
+import java.util.List;
+
+import com.ea.entity.Class;
+
+/**
+ * @author Lewis.Liu
+ */
+public interface ClassDao {
+
+    List<Class> listAllClasses();
+}
---
+++ src/com/ea/dao/UserDao.java
@@ -0,0 +1,24 @@
+// Copyright (c) 2018-2018 Lewis.Liu Limited. All rights reserved.
+// ============================================================================
+// CURRENT VERSION EA.1.0.0
+// ============================================================================
+// CHANGE LOG
+// EA.1.0.0 : 2018-3-15, Lewis.Liu created
+// ============================================================================
+package com.ea.dao;
+
+import java.util.List;
+
+import com.ea.entity.User;
+
+/**
+ * @author Lewis.Liu
+ */
+public interface UserDao {
+
+    List<User> listAllUsers();
+
+    User login(String loginId, String password);
+
+    void logout(User loginUser);
+}
