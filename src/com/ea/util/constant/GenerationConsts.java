029701f32561c16b536c1b00d1e8306504a1f4cf
lewis.liu <1019175915@qq.com>
2018-03-25 16:02:18 +0800

    update generation consts

--- src/com/ea/util/constant/GenerationConsts.java
+++ src/com/ea/util/constant/GenerationConsts.java
@@ -13,12 +13,12 @@ package com.ea.util.constant;
 public class GenerationConsts {
 
     // const variables for generating sql
-    public static final String SQL_INSERT_INTO_SELECT = "insert into %1$s%2$s %3$s %n select %4$s;";
+    public static final String SQL_INSERT_INTO_SELECT = "insert into %1$s%2$s %3$s \n select %4$s;";
     public static final String SQL_SELECT = "select %1$s from %2$s%3$s%4$s;";
     public static final String SQL_WHERE_RESTRICTION = " where %1$s";
     public static final String SQL_RESTRICTION = "%1$s.%2$s = ? and ";
     public static final String SQL_FIELD_COMMA = "%1$s, ";
-    public static final String SQL_FIELD_AS_ALIAS = "%1$s.%2$s as %3$s,%n";
+    public static final String SQL_FIELD_AS_ALIAS = "%1$s.%2$s as %3$s,\n";
     public static final String SQL_FIELD_PARENTHESES = "(%1$s)";
     public static final String SQL_PREPARED_FIELD_COMMA = "?, ";
     public static final String PREFIX_FOR_TABLE_NAME = "ea_";
@@ -39,18 +39,20 @@ public class GenerationConsts {
     public static final String CODE_PACKAGE_FORM = "package %1$s.form;\n";
     public static final String CODE_IMPORT_FORM = "\nimport java.io.Serializable;\n";
     public static final String SEPARATOR_DOT = ".";
-    public static final String CONSTS_FILE_TEMPLATE = "\npublic class %1$sConsts {%n%n%2$s}";
-    public static final String FORM_FILE_TEMPLATE = "\npublic class %1$sForm implements Serializable {%n%n%2$s}";
+    public static final String CONSTS_FILE_TEMPLATE = "\npublic class %1$sConsts {\n\n%2$s\n\tprivate %1$sConsts() {}\n}";
+    public static final String FORM_FILE_TEMPLATE = "\npublic class %1$sForm implements Serializable {\n\n%2$s}";
     public static final String FIELD_IN_CONSTS = "\tpublic static final String %1$s = \"%2$s\";\n";
-    public static final String FIELD_IN_FORM = "\tprivate String %1$s;%n";
-    public static final String SETTER_METHOD = "\n\tpublic void set%1$s(String %2$s) {%n\t\tthis.%2$s = %2$s;\n\t}\n";
-    public static final String GETTER_METHOD = "\n\tpublic String get%1$s() {%n\t\treturn %2$s;\n\t}\n";
+    public static final String FIELD_IN_FORM = "\tprivate String %1$s;\n";
+    public static final String SETTER_METHOD = "\n\tpublic void set%1$s(String %2$s) {\n\t\tthis.%2$s = %2$s;\n\t}\n";
+    public static final String GETTER_METHOD = "\n\tpublic String get%1$s() {\n\t\treturn %2$s;\n\t}\n";
 
     // key to get the absolute path of project
     public static final String KEY_USER_DIR = "user.dir";
 
     public static final String SEPARATOR_PATH = "\\";
-    public static final String SUFFIX_FOR_CONSTS_PATH = "Consts.java";
-    public static final String SUFFIX_FOR_FORM_PATH = "Form.java";
+    public static final String SUFFIX_FOR_CONSTS_PATH = "\\constant";
+    public static final String SUFFIX_FOR_FORM_PATH = "\\form";
+    public static final String SUFFIX_FOR_CONSTS_NAME = "Consts.java";
+    public static final String SUFFIX_FOR_FORM_NAME = "Form.java";
     public static final String UTF_8 = "UTF-8";
 }
