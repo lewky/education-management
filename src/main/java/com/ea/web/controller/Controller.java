f478fcb5fcd8a7158933abb106c7329949a6e4cb
lewis.liu <1019175915@qq.com>
2018-04-03 23:57:32 +0800

    add controller & filter

---
+++ src/main/java/com/ea/web/controller/Controller.java
@@ -0,0 +1,20 @@
+// Copyright (c) 2018-2018 Lewis.Liu Limited. All rights reserved.
+// ============================================================================
+// CURRENT VERSION EA.1.0.0
+// ============================================================================
+// CHANGE LOG
+// EA.1.0.0 : 2018年3月25日, Lewis.Liu created
+// ============================================================================
+package com.ea.web.controller;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author Lewis.Liu
+ *
+ */
+public class Controller extends HttpServlet {
+
+    private static final long serialVersionUID = -4000136003111771574L;
+
+}
--- src/main/java/com/ea/web/filter/CharacterEncodingFilter.java
+++ src/main/java/com/ea/web/filter/CharacterEncodingFilter.java
@@ -1,20 +1,16 @@
 // Copyright (c) 2018-2018 Lewis.Liu Limited. All rights reserved.
 // ============================================================================
-// CURRENT VERSION EA.1.0.0
+// CURRENT VERSION ea.1.0.0
 // ============================================================================
 // CHANGE LOG
-// EA.1.0.0 : 2018年3月25日, Lewis.Liu created
+// ea.1.0.0 : 2018-4-03, Lewis.Liu created
 // ============================================================================
-package com.ea.web;
-
-import javax.servlet.http.HttpServlet;
+package com.ea.web.filter;
 
 /**
  * @author Lewis.Liu
  *
  */
-public class Controller extends HttpServlet {
-
-    private static final long serialVersionUID = -4000136003111771574L;
+public class CharacterEncodingFilter {
 
 }
