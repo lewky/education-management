880de762a03f3aae254b34cf198e6987c94f8085
lewis.liu <1019175915@qq.com>
2018-04-04 23:49:32 +0800

    update charater encoding filter

--- src/main/java/com/ea/web/filter/CharacterEncodingFilter.java
+++ src/main/java/com/ea/web/filter/CharacterEncodingFilter.java
@@ -7,10 +7,49 @@
 // ============================================================================
 package com.ea.web.filter;
 
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
 /**
+ * Set charater encoding (default is UTF-8) for every request.
  * @author Lewis.Liu
  *
  */
-public class CharacterEncodingFilter {
+@WebFilter(urlPatterns = {"/"}, servletNames = "encoding")
+public class CharacterEncodingFilter implements Filter {
+
+    private String encoding;
+
+    /* (non-Javadoc)
+     * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+     */
+    @Override
+    public void init(final FilterConfig filterConfig) throws ServletException {
+        encoding = "UTF-8";
+    }
+
+    /* (non-Javadoc)
+     * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
+     */
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
+            throws IOException, ServletException {
+
+        request.setCharacterEncoding(encoding);
+        chain.doFilter(request, response);
+    }
+
+    /* (non-Javadoc)
+     * @see javax.servlet.Filter#destroy()
+     */
+    @Override
+    public void destroy() {}
 
 }
